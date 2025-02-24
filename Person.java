1876dcb191273ffaab4d3e10caa84d25c92fd2a0
Arjun Dhadwal <adhadwal27@students.polytechnic.org>
2025-02-24 09:09:49 -0800

    Person.java finished

--- Person.java
+++ Person.java
@@ -1,3 +1,25 @@
 public class Person{
-    
+    //fields
+    private boolean direction; //true = right, false = left
+    private int destination; 
+    private int startLoc;
+    //constructors
+    public Person(int myDestination, int myStartingLoc){
+        destination = myDestination;
+        startLoc = myStartingLoc;
+        direction = (destination>startLoc);
+        }
+    //methods
+    public int getDestination(){
+        return destination;
+    }
+    public boolean getDirection(){
+        return direction;
+    }
+    public int getStartLoc(){
+        return startLoc;
+    }
+    public String toString(){
+        return super.toString() + "Start:  " + startLoc + " Destination: " +destination + "Going right? "+ direction;
+    }
 }
