386821476ed24b81fe766c13fcf2a9e5e4d1725b
Arjun Dhadwal <adhadwal27@students.polytechnic.org>
2025-02-27 14:53:42 -0800

    Started working on simulator.java

--- Simulator.java
+++ Simulator.java
@@ -1,3 +1,14 @@
-public class Simulator {
-    
+import java.util.*;
+public class Simulator { //the main class that contains everything
+
+    //fields
+    private ArrayList<Station> stations = new ArrayList<Station>();
+
+    //constructors
+    public Simulator(){
+        for(int i = 0; i<32; i++){
+            Station s = new Station(i);
+            stations.add(s);
+        }
+    }
 }
