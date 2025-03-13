bd7e904b96075f35f4f66811703afcc92ddeac11
Arjun Dhadwal <adhadwal27@students.polytechnic.org>
2025-03-13 16:47:12 -0700

    Added numcycles functionality and resolved issues within README

--- Tester.java
+++ Tester.java
@@ -1,8 +1,9 @@
 public class Tester {
     public static void main(String[] args){
+        final int NUMCYCLES = 10; //number of cycles or "ticks" in the simulator 
         Simulator s = new Simulator(); //Creating the simulator
         s.populate(10, 5); //creating 10 people and 5 cars
-        for(int i = 0; i<32; i++){ //looping the tick several times
+        for(int i = 0; i<NUMCYCLES; i++){ //looping the tick several times
             System.out.println("Cycle: "+i);
             s.tick();
             System.out.println(s.toString());
