d96ea82778381f665c3d4316a8177ace0ed54ee3
Arjun Dhadwal <adhadwal27@students.polytechnic.org>
2025-03-14 10:20:01 -0700

    Testing algorithm functional

--- Tester.java
+++ Tester.java
@@ -11,12 +11,12 @@ public class Tester {
     double maximumValue = 0;
     int carsatMaximumValue = 0;
     for(int i = carTestRange_LOW; i<=carTestRange_HIGH; i++){ //looping through simulations and saving values when appropriate
-        double test = runTrial(64, numpass, i);
-        if(test>maximumValue){
-            maximumValue = test;
+        double passcomp = runTrial(64, numpass, i);
+        if(passcomp/(double)i>maximumValue){
+            maximumValue = passcomp/i;
             carsatMaximumValue = i;
         }
-        System.out.println("Ran test with "+i+" cars, passengers transported per car equals "+test+" with "+numpass+" passengers in the simulation");
+        System.out.println("Ran test with "+i+" cars, passengers transported per car equals "+passcomp/i+" with "+numpass+" passengers in the simulation");
     }
     System.out.println("Most ideal number of cars = "+carsatMaximumValue+" transporting "+maximumValue+" passengers per car.");
     input.close();
@@ -26,17 +26,17 @@ public class Tester {
      * @param cycles number of "ticks" in every simulatioon
      * @param numPassengers the amount of passengers to spawn (usually constant)
      * @param numCars the amount of cars to be spawned (run a loop to optimize)
-     * @return the double value representing how many passengers get successfully transported per car
+     * @return the int value representing how many passengers get successfully transported
      */
-    public static double runTrial(int cycles, int numPassengers, int numCars){
+    public static int runTrial(int cycles, int numPassengers, int numCars){
         int NUMCYCLES = cycles; //number of cycles or "ticks" in the simulator 
         Simulator p = new Simulator(); //Creating the simulator
         p.populate(numPassengers, numCars); //creating 10 people and 5 cars
-        for(int i = 0; i<NUMCYCLES; i++){ //looping the tick several times
+        for(int i = 1; i<=NUMCYCLES; i++){ //looping the tick several times
             System.out.println("Cycle: "+i);
             p.tick();
         }
-        double pc = p.passCompletedPerCar();
+        int pc = p.passCompletedTotal();
         System.out.println(pc+" passengers succesfully transported per car @ "+numCars+" cars and"+numPassengers+" passengers");
         return pc;
     }
