afcc4fb62bfe4e2f00086b04ff071d3cf29908e0
Arjun Dhadwal <adhadwal27@students.polytechnic.org>
2025-03-14 15:16:42 -0700

    User choice functionality added

--- Tester.java
+++ Tester.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class Tester {
     public static void main(String[] args){ //The main method runs a loop to optimize number of cars to passengers completed.
     Scanner input = new Scanner(System.in);
+    System.out.println("Run one simulation or collect data from many? (one/many)");
+    if(input.next().equals("many")){
     System.out.println("Input the lower limit of the number of cars to optimize. Recommended at least 5 so that the program does not use only one car to cut costs the most."); //Scanning for custom parameters
     int carTestRange_LOW = input.nextInt();
     System.out.println("Input the upper limit of the number of cars to optimize");
@@ -10,9 +12,9 @@ public class Tester {
     int numpass = input.nextInt();
     double maximumValue = 0; //highest value of people per car
     int carsatMaximumValue = 0; //how many cars at best people per car
-    int maxProfit = 0;
+    int maxProfit = 0; //maximum profit value saved here
     int profit = 0; //profit value
-    int carsatMaxProfit = 0;
+    int carsatMaxProfit = 0; //car count at the maximum profit value
     int carCost = 1; //cost of the car to operate
     int passengerRevenue = 2; //money gained from ferrying passengers to their station succesffully
 
@@ -36,7 +38,21 @@ public class Tester {
     System.out.println("Most profitable car count: "+carsatMaxProfit+" making $"+maxProfit+" for the rideshare company");
     System.out.println("-----------------------");
     input.close();
+    } else {
+        int NUMCYCLES = 32; //number of cycles or "ticks" in the simulator 
+        Simulator p = new Simulator(); //Creating the simulator
+        System.out.println("How many people would you like to spawn?");
+        int pCount = input.nextInt();
+        System.out.println("How many cars would you like to spawn?");
+        int cCount = input.nextInt();
+        p.populate(pCount, cCount);
+        for(int i = 1; i<=NUMCYCLES; i++){ //looping the tick several times
+            p.tick();
+            System.out.println(p.toString());
+        }
+        input.close();
     }
+}
     /**
      * Method to run a simulation repeatedly for use in a loop to optimize number of cars
      * @param cycles number of "ticks" in every simulatioon
@@ -47,13 +63,13 @@ public class Tester {
     public static int runTrial(int cycles, int numPassengers, int numCars){
         int NUMCYCLES = cycles; //number of cycles or "ticks" in the simulator 
         Simulator p = new Simulator(); //Creating the simulator
-        p.populate(numPassengers, numCars); //creating 10 people and 5 cars
+        p.populate(numPassengers, numCars); //creating people and cars
         for(int i = 1; i<=NUMCYCLES; i++){ //looping the tick several times
             //System.out.println("Cycle: "+i);
             p.tick();
         }
         int pc = p.passCompletedTotal();
-        System.out.println(pc+" passengers succesfully transported per car @ "+numCars+" cars and"+numPassengers+" passengers");
+        System.out.println(pc+" passengers succesfully transported per car @ "+numCars+" cars and "+numPassengers+" passengers");
         return pc;
     }
     
